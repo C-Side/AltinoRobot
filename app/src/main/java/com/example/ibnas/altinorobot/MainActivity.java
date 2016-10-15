1f2dd77ac6c76c0c6fd69739ac9fb924aee3aa50
Ibnu <ibnu6227@gmail.com>
2016-10-15 16:29:22 +0900

    Copy-Pasting activity develop branch sensor

--- app/src/main/java/com/example/ibnas/altinorobot/MainActivity.java
+++ app/src/main/java/com/example/ibnas/altinorobot/MainActivity.java
@@ -194,42 +194,52 @@ public class MainActivity extends AppCompatActivity {
 
         // Defines the action of turning on Buzzer
         bt_loop = (ToggleButton) findViewById(R.id.bt_loop);
+        sendBuf_byte[21] = 10;
         bt_loop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             final Handler handler = new Handler();
             boolean bt_loop_check = false;
-            int i = 0;
+            boolean driving = false;
             Runnable run = new Runnable() {
                 @Override
                 public void run() {
                     if (bt_loop_check) {
-                        int array[] = {37,39,41,42,44,46,48,49};
-                        int arrayLED[] = {0x20,0x02,0x01,0x10,0x40,0x04,0x08,0x80};
-
-                        int arrayDot_1[] = {0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-                        int arrayDot_2[] = {0x00,0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF,0x00,0x00,0x00,0x00,0x00,0x00};
-                        int arrayDot_3[] = {0x00,0x00,0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF,0x00,0x00,0x00,0x00,0x00};
-                        int arrayDot_4[] = {0x00,0x00,0x00,0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF,0x00,0x00,0x00,0x00};
-                        int arrayDot_5[] = {0x00,0x00,0x00,0x00,0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF,0x00,0x00,0x00};
-                        int arrayDot_6[] = {0x00,0x00,0x00,0x00,0x00,0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF,0x00,0x00};
-                        int arrayDot_7[] = {0x00,0x00,0x00,0x00,0x00,0x00,0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF,0x00};
-                        int arrayDot_8[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xFF,0x00,0xFF,0x99,0x66,0x00,0xFF,0x40,0x20,0x10,0x08,0x04,0x02,0x01,0xFF,0x00,0xFF,0x01,0x01,0xFF};
-
-                        sendBuf_byte[4] = 1;
-                        sendBuf_byte[13] = (byte) arrayDot_1[i];
-                        sendBuf_byte[14] = (byte) arrayDot_2[i];
-                        sendBuf_byte[15] = (byte) arrayDot_3[i];
-                        sendBuf_byte[16] = (byte) arrayDot_4[i];
-                        sendBuf_byte[17] = (byte) arrayDot_5[i];
-                        sendBuf_byte[18] = (byte) arrayDot_6[i];
-                        sendBuf_byte[19] = (byte) arrayDot_7[i];
-                        sendBuf_byte[20] = (byte) arrayDot_8[i];
-                        sendBuf_byte[21] = 10;
-                        sendBuf_byte[22] = 0;
-                        //sendBuf_byte[23] = (byte) arrayLED[i];
-                        sendByte(sendBuf_byte);
-                        i++;
-                        if (i == arrayDot_1.length) i = 0;
-                        handler.postDelayed(this,500);
+                        if (value_s2 < 50 && !driving) {
+                            sendBuf_byte[4] = 1;
+                            sendBuf_byte[5] = 2;
+                            sendBuf_byte[6] = 0;
+                            sendBuf_byte[7] = (byte) (240 / 256);
+                            sendBuf_byte[8] = (byte) (240 % 256);
+                            sendBuf_byte[9] = 0;
+                            sendBuf_byte[10] = (byte) (240 / 256);
+                            sendBuf_byte[11] = (byte) (240 % 256);
+                            sendBuf_byte[23] = (byte) (0x03);
+                            sendByte(sendBuf_byte);
+                            driving = true;
+                        } else if (value_s2 >= 50 && driving) {
+                            sendBuf_byte[4] = 1;
+                            sendBuf_byte[5] = 2;
+                            sendBuf_byte[6] = 0;
+                            sendBuf_byte[7] = (byte) ((32768 + 240) / 256);
+                            sendBuf_byte[8] = (byte) ((32768 + 240) % 256);
+                            sendBuf_byte[9] = 0;
+                            sendBuf_byte[10] = (byte) ((32768 + 240) / 256);
+                            sendBuf_byte[11] = (byte) ((32768 + 240) % 256);
+                            sendBuf_byte[23] = (byte) (0xC0);
+                            sendByte(sendBuf_byte);
+                            
+                            sendBuf_byte[4] = 1;
+                            sendBuf_byte[5] = 2;
+                            sendBuf_byte[6] = 0;
+                            sendBuf_byte[7] = 0;
+                            sendBuf_byte[8] = 0;
+                            sendBuf_byte[9] = 0;
+                            sendBuf_byte[10] = 0;
+                            sendBuf_byte[11] = 0;
+                            sendBuf_byte[23] = 0;
+                            sendByte(sendBuf_byte);
+                            driving = false;
+                        }
+                        handler.postDelayed(this,10);
                     }
                 }
             };
@@ -242,19 +252,8 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     handler.removeCallbacks(run);
                     bt_loop_check = false;
-                    sendBuf_byte[4] = 1;
-                    sendBuf_byte[13] = 0;
-                    sendBuf_byte[14] = 0;
-                    sendBuf_byte[15] = 0;
-                    sendBuf_byte[16] = 0;
-                    sendBuf_byte[17] = 0;
-                    sendBuf_byte[18] = 0;
-                    sendBuf_byte[19] = 0;
-                    sendBuf_byte[20] = 0;
-                    sendBuf_byte[22] = 0;
-                    sendBuf_byte[23] = 0;
-                    sendByte(sendBuf_byte);
-                    i = 0;
+                    driving = false;
+                    bt_stop.performClick();
                 }
             }
         });
@@ -320,9 +319,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 cds.setText(""+((readBuf_byte[23] & 0xff)*256+(readBuf_byte[24] & 0xff)));
                 value_s1 = (readBuf_byte[7] & 0xff)*256+(readBuf_byte[8] & 0xff);
-                ir1.setText(value_s1);
+                ir1.setText(""+(value_s1));
                 value_s2 = (readBuf_byte[9] & 0xff)*256+(readBuf_byte[10] & 0xff);
-                ir2.setText(value_s2);
+                ir2.setText(""+(value_s2));
                 ir3.setText(""+((readBuf_byte[11] & 0xff)*256+(readBuf_byte[12] & 0xff)));
                 ir4.setText(""+((readBuf_byte[13] & 0xff)*256+(readBuf_byte[14] & 0xff)));
                 ir5.setText(""+((readBuf_byte[15] & 0xff)*256+(readBuf_byte[16] & 0xff)));
